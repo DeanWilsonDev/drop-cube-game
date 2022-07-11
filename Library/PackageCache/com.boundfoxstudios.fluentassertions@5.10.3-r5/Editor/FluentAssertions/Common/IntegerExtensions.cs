7e14293b94a49477c88223cae9a47a7992f68b87
Dean Wilson <deanwilson@proton.me>
2022-07-11 19:37:39 +0800

    Remove ignored files

--- Library/PackageCache/com.boundfoxstudios.fluentassertions@5.10.3-r5/Editor/FluentAssertions/Common/IntegerExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FluentAssertions.Common
-{
-    internal static class IntegerExtensions
-    {
-        public static string Times(this int count) => count == 1 ? "1 time" : $"{count} times";
-    }
-}
--- Library/PackageCache/com.boundfoxstudios.fluentassertions@5.10.3-r5/Editor/FluentAssertions/Execution/MSpecFramework.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FluentAssertions.Execution
-{
-    internal class MSpecFramework : LateBoundTestFramework
-    {
-        protected override string AssemblyName => "Machine.Specifications";
-
-        protected override string ExceptionFullName => "Machine.Specifications.SpecificationException";
-    }
-}
