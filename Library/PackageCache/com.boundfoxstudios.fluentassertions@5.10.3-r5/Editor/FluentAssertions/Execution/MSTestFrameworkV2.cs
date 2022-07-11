2721b5574c8dbbbf6adcb3b7a271558ab8796f59
Dean Wilson <deanwilson@proton.me>
2022-07-11 19:26:39 +0800

    Remove ignored files

--- Library/PackageCache/com.boundfoxstudios.fluentassertions@5.10.3-r5/Editor/FluentAssertions/Execution/MSTestFrameworkV2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FluentAssertions.Execution
-{
-    internal class MSTestFrameworkV2 : LateBoundTestFramework
-    {
-        protected override string ExceptionFullName => "Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException";
-
-        protected override string AssemblyName => "Microsoft.VisualStudio.TestPlatform.TestFramework";
-    }
-}
