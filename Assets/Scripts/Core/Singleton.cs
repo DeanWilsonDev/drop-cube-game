fb2b81eb2abe9941d6c8c30cd648c7fabd7a73c2
Dean Wilson <deanwilson@proton.me>
2022-07-13 19:37:32 +0800

    Add Touch Controls, Input System, and Generic MonoBehaviour Singleton

---
+++ Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace BlackPad.Core
+{
+    public class Singleton<T> : MonoBehaviour where T: Component {
+
+        static T _instance;
+
+        public static T Instance {
+            get {
+                if (_instance != null) return _instance;
+                var obj = new GameObject {
+                    name = typeof(T).Name,
+                    hideFlags = HideFlags.HideAndDontSave
+                };
+                _instance = obj.AddComponent<T>();
+                return _instance;
+            }
+        }
+
+        void OnDestroy() {
+            if (_instance == this) {
+                _instance = null;
+            }
+        }
+
+    }
+}
