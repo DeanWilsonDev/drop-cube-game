7938a4075d7798c3ec5ae57af5c3a1bc84f73c17
Dean Wilson <deanwilson@proton.me>
2022-07-28 17:27:27 +0800

    add smoother camera movement

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -6,24 +6,33 @@ namespace BlackPad.DropCube.Camera {
 
     [SerializeField] float speed;
     GameObject _player;
-
+    [SerializeField] float smoothTime = 0.3f;
+    [SerializeField] float threshold;
+    Vector3 velocity = Vector3.zero;
     UnityEngine.Camera _camera;
 
     // Start is called before the first frame update
     void Start() {
       _player = GameObject.FindWithTag("Player");
-      _camera = GetComponent<UnityEngine.Camera>();
+      _camera = UnityEngine.Camera.main;
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
       var playerTransformPosition = _player.transform.position;
-      var topOfScreenInWorldPosition =
-        _camera.ScreenToWorldPoint(new Vector3(0, Screen.height, _camera.nearClipPlane));
-      Debug.Log("camera" + topOfScreenInWorldPosition);
-      Debug.Log("Player" + playerTransformPosition);
-      var distanceFromBottom = topOfScreenInWorldPosition.y - playerTransformPosition.y;
-      transform.Translate(Vector3.down * ((distanceFromBottom + speed) * Time.deltaTime));
+      var bottomOfTheScreen =
+        _camera.ScreenToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane));
+      var cameraTransform = _camera.transform;
+      var cameraTransformPosition = cameraTransform.position;
+      var targetPosition = new Vector3(cameraTransformPosition.x, bottomOfTheScreen.y, cameraTransformPosition.z);
+
+      var topOfScreen = _camera.ScreenToWorldPoint(new Vector3(0, Screen.height, _camera.nearClipPlane));  
+      var distanceFromTopOfScreen = topOfScreen.y - playerTransformPosition.y;
+      _camera.transform.position = Vector3.SmoothDamp(cameraTransformPosition, targetPosition, ref velocity, smoothTime, speed + distanceFromTopOfScreen);
+
+      if (playerTransformPosition.y >= bottomOfTheScreen.y) {
+        // Restart the game somehow
+      }
     }
 
   }
