9b66536f0cf8997e759359eac6d39313e54a3630
Dean Wilson <deanwilson@proton.me>
2022-08-12 19:19:38 +0800

    adjust level properties

--- Assets/Scripts/Level/Room/BackWall/BackWallGenerator.cs
+++ Assets/Scripts/Level/Room/BackWall/BackWallGenerator.cs
@@ -9,6 +9,7 @@ namespace BlackPad.DropCube.Level
 
         public static BackWall InitializeBackWall(Component parent, float roomHeight, float totalRoomWidth) {
             var backWall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            backWall.name = "Back Wall";
             backWall.transform.localScale = new Vector3(totalRoomWidth, roomHeight, 1);
             var position = parent.transform.position;
             backWall.transform.position = new Vector3(
@@ -19,6 +20,7 @@ namespace BlackPad.DropCube.Level
             var renderer = backWall.GetComponent<Renderer>();
             renderer.material.SetColor(Color1, Color.black);
             backWall.AddComponent<BackWall>();
+            backWall.transform.parent = parent.transform;
             return backWall.GetComponent<BackWall>();
         }
     }
